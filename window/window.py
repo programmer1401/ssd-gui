c33f2319332d08b0d4db869c9070138e5f2ed089
programmer1401 <32697340+programmer1401@users.noreply.github.com>
2020-11-30 14:51:21 +0800

    Update window.py

--- window/window.py
+++ window/window.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 
+from gui.window.evalGUI import EvalWin
 from gui.window.testGUI import TestWin
 from gui.window.trainGUI import TrainWin
 
@@ -8,9 +9,9 @@ class Window:
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("基于PyTorch的SSD的研究与实现")
-        # self.window.geometry("1000x618")
+        self.window.geometry("1000x618")
         # 前两个参数是窗口的宽度和高度。 最后两个参数是x和y屏幕坐标。
-        self.window.geometry("1000x618+250+100")
+        # self.window.geometry("2000x1236")
 
         style = ttk.Style()
         style.configure('TNotebook.Tab', font=('微软雅黑', '11'),padding=[5, 5, 5, 5], width=10)
@@ -22,8 +23,12 @@ class Window:
         TrainWin(self.frame1)
 
         self.frame2 = tk.Frame(self.tab)
-        self.tab2 = self.tab.add(self.frame2, text="实时检测")
-        TestWin(self.frame2)
+        self.tab2 = self.tab.add(self.frame2, text="模型验证")
+        EvalWin(self.frame2)
+
+        self.frame3 = tk.Frame(self.tab)
+        self.tab3 = self.tab.add(self.frame3, text="实时检测")
+        TestWin(self.frame3)
 
         self.tab.pack(expand=True, fill=tk.BOTH)
 
