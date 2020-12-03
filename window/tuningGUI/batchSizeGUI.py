0fdf05be6a62c4cd3e2e891552b2d321030bc6bf
programmer1401 <32697340+programmer1401@users.noreply.github.com>
2020-12-03 12:14:00 +0800

    Update batchSizeGUI.py

--- window/tuningGUI/batchSizeGUI.py
+++ window/tuningGUI/batchSizeGUI.py
@@ -6,11 +6,11 @@ import time
 from gui.trainData import TrainData
 from gui.utils import CreateCanvas
 
-basenet = "vgg16_reducedfc.pth"
 learning_rate = 0.0002
 
 ERROR_TITLE = ["输入错误！", "运行错误！"]
 INFO_TITLE = ["状态信息", "提示"]
+
 
 class batchSizeGUI():
     def __init__(self, window):
@@ -21,8 +21,6 @@ class batchSizeGUI():
         self.createWidget()
         # self.window.mainloop()
 
-        self.time_ass = []
-        self.loss_ass = []
         # 标志位，程序是否执行，用于设置 “切换”按钮能否正常使用
         # 0：没有执行过，因此点击按钮需弹出错误提示框
         # 1：第一次执行，不需要清空canvas画布
@@ -38,69 +36,85 @@ class batchSizeGUI():
               font=("STSONG", 13)).place(x=100, y=60)
 
         # 输入
-        Label(self.window, text="迭代次数：", font=("STSONG", 14)).place(x=90, y=150)
-        Label(self.window, text="学习率大小：", font=("STSONG", 14)).place(x=90, y=200)
-        Label(self.window, text="是否使用cuda：", font=("STSONG", 14)).place(x=90, y=250)
-        Label(self.window, text="请选择 batch_size 的范围：", font=("STSONG", 14)).place(x=90, y=300)
-        Label(self.window, text="起始值：", font=("STSONG", 14)).place(x=120, y=350)
-        Label(self.window, text="终止值：", font=("STSONG", 14)).place(x=120, y=390)
-        Label(self.window, text="增长间隔：", font=("STSONG", 14)).place(x=120, y=430)
+        Label(self.window, text="主干网络：", font=("STSONG", 14)).place(x=90, y=150)
+        Label(self.window, text="迭代次数：", font=("STSONG", 14)).place(x=90, y=200)
+        Label(self.window, text="学习率大小：", font=("STSONG", 14)).place(x=90, y=250)
+        Label(self.window, text="是否使用cuda：", font=("STSONG", 14)).place(x=90, y=300)
+        Label(self.window, text="请选择 batch_size 的范围：", font=("STSONG", 14)).place(x=90, y=350)
+        Label(self.window, text="起始值：", font=("STSONG", 14)).place(x=120, y=400)
+        Label(self.window, text="终止值：", font=("STSONG", 14)).place(x=120, y=440)
+        Label(self.window, text="增长间隔：", font=("STSONG", 14)).place(x=120, y=480)
+
+        basenet = IntVar()
+        basenet.set(1)
+        Radiobutton(self.window, text="VGG", value=1, variable=basenet, font=("STSONG", 14)).place(x=250, y=150)
+        Radiobutton(self.window, text="ResNet", value=2, variable=basenet, font=("STSONG", 14)).place(x=330, y=150)
 
         # 迭代次数
         iter_num = Entry(self.window, textvariable=StringVar(value="10"))
-        iter_num.place(x=250, y=150)
-        Label(self.window, text=str(learning_rate) + "（最优值）").place(x=250, y=200)
+        iter_num.place(x=250, y=200)
+
+        # 学习率
+        learning_rate = Entry(self.window, textvariable=StringVar(value="0.0002"))
+        learning_rate.place(x=250, y=250)
 
         # 是否使用 cuda
         cuda = IntVar()
-        cuda.set(1)
-        Radiobutton(self.window, text="是", value=1, variable=cuda).place(x=250, y=250)
-        Radiobutton(self.window, text="否", value=2, variable=cuda).place(x=300, y=250)
+        cuda.set(2)
+        Radiobutton(self.window, text="是", value=1, variable=cuda).place(x=250, y=300)
+        Radiobutton(self.window, text="否", value=2, variable=cuda).place(x=330, y=300)
 
         # 选择 batch_size 范围
         start = Entry(self.window, textvariable=StringVar(value="1"))
-        start.place(x=250, y=350)
+        start.place(x=250, y=400)
         end = Entry(self.window, textvariable=StringVar(value="10"))
-        end.place(x=250, y=390)
+        end.place(x=250, y=440)
         step = Entry(self.window, textvariable=StringVar(value="1"))
-        step.place(x=250, y=430)
+        step.place(x=250, y=480)
 
         # 按钮
         Button(self.window, text="开始", height="1", width="15",
-               command=lambda: self.tuning(int(iter_num.get()), int(start.get()), int(end.get()),
-                                           int(step.get()), cuda.get())).place(x=90, y=500)
+               command=lambda: self.tuning(basenet.get(), int(iter_num.get()),
+                                           float(learning_rate.get()), cuda.get(),
+                                           int(start.get()), int(end.get())+1, int(step.get())))\
+            .place(x=90, y=530)
 
         self.change_img = Button(self.window, text="切换图像：运行速度", height="1", width="15",
-               command=lambda: self.create_matplotlib())
-        self.change_img.place(x=290, y=500)
+                                 command=lambda: self.create_matplotlib())
+        self.change_img.place(x=290, y=530)
 
         self.matplot = CreateCanvas(self.window)
         self.canvas = self.matplot.get_canvas()
         self.figure = self.matplot.get_figure()
 
-    def tuning(self, iter_num, start, end, step, cuda):
-        self.time_ass = []
-        self.loss_ass = []
+    def tuning(self, basenet, iter_num, learning_rate, cuda, start, end, step):
+        try:
+            self.time_ass = []
+            self.loss_ass = []
 
-        self.start = start
-        self.end = end
-        self.step = step
+            self.start = start
+            self.end = end
+            self.step = step
 
-        cuda = True if cuda == 1 else False
+            basenet = "vgg16_reducedfc.pth" if basenet == 1 else 'resnet50-19c8e357.pth'
+            cuda = True if cuda == 1 else False
 
-        for batch_size in range(start, end, step):
-            traindata = TrainData(iter_num, basenet=basenet, batch_size=int(batch_size), cuda=cuda,
-                                  lr=learning_rate)
-            t0 = time.time()
-            loss = traindata.train()
-            t1 = time.time()
+            for batch_size in range(start, end, step):
+                traindata = TrainData(iter_num, basenet=basenet, batch_size=int(batch_size), cuda=cuda,
+                                      lr=learning_rate)
+                t0 = time.time()
+                loss = traindata.train()
+                t1 = time.time()
 
-            self.time_ass += [t1 - t0]
-            self.loss_ass += [loss[-1]]
+                self.time_ass += [t1 - t0]
+                self.loss_ass += [loss[-1]]
 
-        self.finish += 1
-        # 默认显示损失函数图像
-        self.create_matplotlib()
+            self.finish += 1
+            # 默认显示损失函数图像
+            self.create_matplotlib()
+
+        except Exception as e:
+            messagebox.showerror(title="运行错误！", message=str(e))
 
     def create_matplotlib(self):
         if self.finish == 0:
