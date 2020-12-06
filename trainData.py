db3fdae17aa335579185d021bdd0890f2803e9c2
programmer1401 <32697340+programmer1401@users.noreply.github.com>
2020-12-06 08:50:10 +0800

    Update trainData.py
    add conf_loss_ass, loc_loss_ass to compare the difference of vgg and resnet

--- trainData.py
+++ trainData.py
@@ -1,9 +1,10 @@
+import visdom
+
 from data import *
-from layers.img_utils import show_loss
-from resnet_ssd import build_resnet_ssd
+from resnet_ssd import build_ssd
 from utils.augmentations import SSDAugmentation
 from layers.modules import MultiBoxLoss
-from ssd import build_ssd
+# from vgg import build_ssd
 import os
 import time
 import torch
@@ -17,28 +18,33 @@ import torch.utils.data as data
 import warnings
 warnings.filterwarnings("ignore")
 
+viz = visdom.Visdom()
+
 os.environ['CUDA_ENABLE_DEVICES'] = '0'
 
 class TrainData:
-    def __init__(self, iter_num, dataset='VOC', dataset_root=VOC_ROOT,
+    def __init__(self, iter_num, dataset='VOC', dataset_root=VOC_ROOT, model="vgg",
                  basenet='vgg16_reducedfc.pth', batch_size=4, cuda=False,
                  lr=1e-4, momentum=0.9, weight_decay=5e-4, gamma=0.1,
-                 save_folder="E:\Code\Examples\Gao\ssd.pytorch-master\weights/"):
-        self.dataset_name = dataset
-        self.dataset_root = dataset_root
+                 save_folder="D:\WorkSpace\PyCharmSpace\SSD\ssd.pytorch-master\weights\\"):
+
         self.basenet = basenet
-        self.iter_num = iter_num
         self.batch_size = batch_size
         self.cuda = cuda
+        self.dataset = dataset
+        self.dataset_root = dataset_root
+        self.gamma = gamma
+        self.iter_num = iter_num
         self.lr = lr
         self.momentum = momentum
-        self.weight_decay = weight_decay
-        self.gamma = gamma
+        self.model = model
         self.save_folder = save_folder
+        self.weight_decay = weight_decay
+
+        self.visdom = True
 
     def set_equipment(self):
         if torch.cuda.is_available():
-            print(torch.cuda.get_device_name(0))
             if self.cuda:
                 torch.set_default_tensor_type('torch.cuda.FloatTensor')
             if not self.cuda:
@@ -51,10 +57,13 @@ class TrainData:
         if not os.path.exists(self.save_folder):
             os.mkdir(self.save_folder)
 
-    def set_dataset(self):
-        global cfg, dataset
-        if self.dataset_name == 'COCO':
+
+    def train(self):
+        self.set_equipment()
+        if self.dataset == 'COCO':
             if self.dataset_root == VOC_ROOT:
+                # if not os.path.exists(COCO_ROOT):
+                #     parser.error('Must specify dataset_root if specifying dataset')
                 print("WARNING: Using default COCO dataset_root because " +
                       "--dataset_root was not specified.")
                 self.dataset_root = COCO_ROOT
@@ -62,52 +71,39 @@ class TrainData:
             dataset = COCODetection(root=self.dataset_root,
                                     transform=SSDAugmentation(cfg['min_dim'],
                                                               MEANS))
-        elif self.dataset_name == 'VOC':
+        elif self.dataset == 'VOC':
+            # if self.dataset_root == COCO_ROOT:
+            #     parser.error('Must specify dataset if specifying dataset_root')
             cfg = voc
             dataset = VOCDetection(root=self.dataset_root,
                                    transform=SSDAugmentation(cfg['min_dim'],
                                                              MEANS))
 
-        return cfg, dataset
+        # if args.visdom:
+        #     viz = visdom.Visdom()
 
+        ssd_net = build_ssd('train', self.model, cfg['min_dim'], cfg['num_classes'])
+        net = ssd_net
 
-    def set_net(self):
-        global ssd_net, net
-        if self.basenet == 'vgg16_reducedfc.pth':
-            print('Loading base network vgg16...')
-            ssd_net = build_ssd('train', cfg['min_dim'], cfg['num_classes'])
-            net = ssd_net
+        if self.cuda:
+            net = torch.nn.DataParallel(ssd_net)
+            cudnn.benchmark = True
 
-            if self.cuda:
-                net = torch.nn.DataParallel(ssd_net)
-                cudnn.benchmark = False
+        print('Loading base network...')
+        if self.model=="vgg":
+            base_weights = torch.load(self.save_folder +"vgg\\"+ self.basenet)
+        elif self.model=="resnet":
+            base_weights = torch.load(self.save_folder + "resnet\\" + self.basenet)
+        ssd_net.base.load_state_dict(base_weights)
 
-            vgg_weights = torch.load(self.save_folder + self.basenet)
-            ssd_net.vgg.load_state_dict(vgg_weights)
+        if self.cuda:
+            net = net.cuda()
 
-        elif self.basenet == 'resnet50-19c8e357.pth':
-            print('Loading base network resnet50...')
-            ssd_net = build_resnet_ssd('train', 300, num_classes=21)
-            net = ssd_net
-
-            # 为resnet_ssd加载预训练权重文件
-            # 1、加载模型
-            resnet_pretrained_weights = torch.load(self.save_folder + self.basenet)
-            # 2、初始化网络
-            resnet_dict = ssd_net.state_dict()
-            # 3、剔除掉网络中没有的键
-            pretrained_dict_l = {k: v for k, v in resnet_pretrained_weights.items() if k in resnet_dict}
-            # 4、用预训练的权重文件，更新网络权重
-            resnet_dict.update(pretrained_dict_l)
-            # 5、将更新了的参数放入网络中
-            ssd_net.load_state_dict(resnet_dict)
-
-        return ssd_net, net
-
-    def train(self):
-        self.set_equipment()
-        cfg, dataset = self.set_dataset()
-        ssd_net, net = self.set_net()
+        print('Initializing weights...')
+        # initialize newly added layers' weights with xavier method
+        ssd_net.extras.apply(self.weights_init)
+        ssd_net.loc.apply(self.weights_init)
+        ssd_net.conf.apply(self.weights_init)
 
         optimizer = optim.SGD(net.parameters(), lr=self.lr, momentum=self.momentum,
                               weight_decay=self.weight_decay)
@@ -118,18 +114,23 @@ class TrainData:
         # loss counters
         loc_loss = 0
         conf_loss = 0
-        epoch = 0
+
         loss_ass = []
-        print('Loading the dataset...')
+        conf_loss_ass = []
+        loc_loss_ass = []
 
-        epoch_size = len(dataset) // self.batch_size
-        print('Training SSD on:', dataset.name)
-
+        print('Loading the dataset...', 'Training SSD on:', self.dataset)
         step_index = 0
+
+        if self.visdom:
+            vis_title = 'SSD.PyTorch on ' + dataset.name + '-backbone ' + self.model
+            vis_legend = ['Loc Loss', 'Conf Loss', 'Total Loss']
+            iter_plot = self.create_vis_plot('Iteration', 'Loss', vis_title, vis_legend)
 
         data_loader = data.DataLoader(dataset, self.batch_size,
                                       shuffle=True, collate_fn=detection_collate,
                                       pin_memory=True)
+
         # create batch iterator
         batch_iterator = iter(data_loader)
         for iteration in range(self.iter_num):
@@ -139,11 +140,7 @@ class TrainData:
                 self.adjust_learning_rate(optimizer, self.gamma, step_index)
 
             # load train data
-            try:
-                images, targets = next(batch_iterator)
-            except StopIteration as e:
-                batch_iterator = iter(data_loader)
-                images, targets = next(batch_iterator)
+            images, targets = next(batch_iterator)
 
             if self.cuda:
                 images = Variable(images.cuda())
@@ -151,6 +148,7 @@ class TrainData:
             else:
                 images = Variable(images)
                 targets = [Variable(ann, volatile=True) for ann in targets]
+
             # forward
             t0 = time.time()
             out = net(images)
@@ -164,19 +162,19 @@ class TrainData:
             loc_loss += loss_l.item()
             conf_loss += loss_c.item()
 
+            iteration = iteration + 1
             if iteration % 2 == 0:
                 print('timer: %.4f sec.' % (t1 - t0))
                 print('iter ' + repr(iteration) + ' || Loss: %.4f ||' % (loss.item()), end=' ')
+
+            if self.visdom:
+                self.update_vis_plot(iteration, loss_l.item(), loss_c.item(), iter_plot, None, 'append')
+
+            loc_loss_ass = loc_loss_ass + [loss_l.item()]
+            conf_loss_ass = conf_loss_ass + [loss_c.item()]
             loss_ass = loss_ass + [loss.item()]
 
-            if iteration != 0 and iteration % 5 == 0:
-                print('Saving state, iter:', iteration)
-                # torch.save(ssd_net.state_dict(), 'E:\Code\Examples\Gao\ssd.pytorch-master\weights/ssd300_VOC_' +
-                #            repr(iteration) + '.pth')
-
-        torch.save(ssd_net.state_dict(), self.save_folder + '' + self.dataset_name + '.pth')
-        return loss_ass
-
+        return loc_loss_ass, conf_loss_ass, loss_ass
 
     def adjust_learning_rate(self, optimizer, gamma, step):
         """Sets the learning rate to the initial LR decayed by 10 at every
@@ -189,11 +187,44 @@ class TrainData:
             param_group['lr'] = lr
 
 
-    def xavier(param):
+    def xavier(self, param):
         init.xavier_uniform(param)
 
-
-    def weights_init(self, m):
+    def weights_init(self,m):
         if isinstance(m, nn.Conv2d):
-            self.xavier(m.weight.data)
-            m.bias.data.zero_()
+            nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            # xavier(m.weight.data)
+            # m.bias.data.zero_()
+        elif isinstance(m, nn.BatchNorm2d):
+            nn.init.constant_(m.weight, 1)
+            nn.init.constant_(m.bias, 0)
+
+    def create_vis_plot(self, _xlabel, _ylabel, _title, _legend):
+        return viz.line(
+            X=torch.zeros((1,)).cpu(),
+            Y=torch.zeros((1, 3)).cpu(),
+            opts=dict(
+                xlabel=_xlabel,
+                ylabel=_ylabel,
+                title=_title,
+                legend=_legend
+            )
+        )
+
+    def update_vis_plot(self, iteration, loc, conf, window1, window2, update_type,
+                        epoch_size=1):
+        viz.line(
+            X=torch.ones((1, 3)).cpu() * iteration,
+            Y=torch.Tensor([loc, conf, loc + conf]).unsqueeze(0).cpu() / epoch_size,
+            win=window1,
+            update=update_type
+        )
+        # initialize epoch plot on first iteration
+        if iteration == 0:
+            viz.line(
+                X=torch.zeros((1, 3)).cpu(),
+                Y=torch.Tensor([loc, conf, loc + conf]).unsqueeze(0).cpu(),
+                win=window2,
+                update=True
+            )
+
